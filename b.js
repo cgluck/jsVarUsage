96f842e00d8377a80df14ec124ea25e01493633b
C Gluck <no@me.com>
2013-11-20 11:17:50 +0100

    fix + controllo variabili globali

--- b.js
+++ b.js
@@ -3,11 +3,15 @@ var b=1;
 
 /*ciao */
 function f3 ()
-function(y)
+function(y){
+	var fg  = 9;
+	f3b();
+}
 
 
 function f1()
 {
+	a = 9;
 	var f1_1;
 	f2 ();
 	return true;
@@ -16,5 +20,6 @@ function f2()
 {
 	var f2_1 = 0;
 	f1();
+	var gggh;
 	return true;
 }
--- script.py
+++ script.py
@@ -1,23 +1,25 @@
 import re
 
+lines = []
 script_file = ''	#variabile globale
 
 def count_functions_usage(filename):
 	fnc = []	#contiene l'elenco delle funzioni
 	fnc_counter = []	#conta le occorrenze delle funzioni trovate
-	lines = []
 	is_cpp_comment = 0
 
-	p = re.compile('function')
-	double_slash = re.compile('//')
-	cpp_comment_a = re.compile('/[*]')
-	cpp_comment_b = re.compile('[*]/')
+	p = re.compile('\\bfunction\\b')
+	double_slash = re.compile('\\b//\\b')
+	cpp_comment_a = re.compile('\\b/[*]\\b')
+	cpp_comment_b = re.compile('\\b[*]/\\b')
 	f = open(filename, 'r')
 
 	global script_file
+	global lines
 	script_file = f.readlines()	#memorizzo tutte le righe nell'array, una riga per elemento
 	f.close()
 	
+	#elimino tutte le righe commentate e memorizzo quelle valide in lines
 	for line in script_file:
 		curr_line = line.strip()
 		m_slash = double_slash.search(curr_line)
@@ -62,7 +64,7 @@ def count_functions_usage(filename):
 	#cerco le varie funzioni fun in tutto il file e mi aspetto di trovare almeno 2 occorrenze (la firma della funzione e almeno una sua chiamata)
 	for fun in fnc:
 		fun_counter = 0
-		p = re.compile(fun, re.M)
+		p = re.compile('\\b'+fun+'\\b')
 		for line in lines:
 			m = p.search(line)
 			if m:
@@ -81,33 +83,36 @@ count_functions_usage('b.js')
 
 global_vars = []
 local_vars = []
-p = re.compile('function')
-vr = re.compile('var')
+p = re.compile('\\bfunction\\b')
+vr = re.compile('\\bvar\\b')
 block_end = re.compile('}')
 is_function = 0
 tmp_local_vars = []
+gvar_counter = []
 
 #cerco le variabili dichiarate
-for line in script_file:
+for line in lines:
 	curr_line = line.strip()
 	m_var = vr.search(curr_line)
 	m_function = p.search(curr_line)
 	m_blockend = block_end.search(curr_line)
 	if m_function:
+		#da qui trovero' variabili locali
 		is_function = 1
 		del tmp_local_vars[:]	#pulisco la lista
 	if m_blockend:
+		#fine variaibli locali per una certa funzione, copio quelle che ho trovato in un elemento della lista (avro' una lista di liste)
 		is_function = 0
 		local_vars.append(list(tmp_local_vars))	#aggiungo alla lista la lista delle variabili locali di una funzione
-		#for i in range(0,len(tmp_local_vars)):
-			#local_vars[len(local_vars)-1][i] = tmp_local_vars[i]
 	if m_var:
+		#trovata una variabile, piu' avanti vedo se salvarla come variabile globale o locale (cioe' di una funzione)
 		if curr_line[m_var.end()] != ';' and curr_line[m_var.end()] != '\n':
 			tmp_string = ''
 			k = m_var.end()
 			while curr_line[k] == ' ' or curr_line[k] == '\t':
 				k = k + 1
 			while curr_line[k] != ';' and curr_line[k] != ' ' and curr_line[k] != '=':
+				#questa e' la variabile, la salvo tutta in tmp_string
 				tmp_string = tmp_string + curr_line[k]
 				k = k + 1
 			if len(tmp_string) > 0:
@@ -117,11 +122,28 @@ for line in script_file:
 				else:
 					#variabile locale di una funzione
 					tmp_local_vars.append(tmp_string)
+					
+#controllo se le variabili dichiarate sono usate
+for gvar in global_vars:
+	gv_counter = 0
+	p = re.compile('\\b'+gvar+'\\b')
+	for line in lines:
+		m = p.search(line)
+		if m:
+			#print 'OK ', fun
+			gv_counter +=1
+		gvar_counter.append(gv_counter)
+	if gv_counter < 2:
+		print 'KO - Occorrenze per', gvar, ':', gv_counter
+	#else:
+		#print 'OK', gvar, ':', gv_counter
+	
 print 'Global'
 for el in global_vars:
 	print el
 print 'Local'
 for el in local_vars:
+	print '---'
 	for el1 in el:
 		print el1
 
