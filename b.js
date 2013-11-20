af5c1b8798a456a508d4d8b4eb731ea2d2f075f0
C Gluck <no@me.com>
2013-11-20 11:44:26 +0100

    controllo variabili locali

--- b.js
+++ b.js
@@ -14,12 +14,18 @@ function f1()
 	a = 9;
 	var f1_1;
 	f2 ();
+	f1_1 = 9;
 	return true;
 }
 function f2()
 {
 	var f2_1 = 0;
 	f1();
+	f4();
 	var gggh;
 	return true;
+}
+function f4()
+{
+	a = 89;
 }
--- script.py
+++ script.py
@@ -75,75 +75,93 @@ def count_functions_usage(filename):
 			print 'KO - Occorrenze per', fun, ':', fun_counter
 		#else:
 			#print 'OK - Occorrenze per', fun, ':', fun_counter
+			
+def count_variables_usage():
+	global_vars = []
+	local_vars = []
+	p = re.compile('\\bfunction\\b')
+	vr = re.compile('\\bvar\\b')
+	block_end = re.compile('}')
+	is_function = 0		#true o false
+	tmp_local_vars = []
+	tmp_function_list = []
+	gvar_counter = []
+	lvar_counter = []
+	function_list = []	#lista di liste che contengono le righe delle funzioni che hanno almeno 1 variabile locale
+	local_var_cnt = 0
+
+	#cerco le variabili dichiarate
+	for line in lines:
+		curr_line = line.strip()
+		m_var = vr.search(curr_line)
+		m_function = p.search(curr_line)
+		m_blockend = block_end.search(curr_line)
+		if is_function == 1:
+			tmp_function_list.append(curr_line)
+		if m_function:
+			#da qui trovero' variabili locali
+			is_function = 1
+			local_var_cnt = 0
+			del tmp_local_vars[:]	#pulisco la lista
+			del tmp_function_list[:]
+			tmp_function_list.append(curr_line)
+		if m_blockend:
+			#fine variaibli locali per una certa funzione, copio quelle che ho trovato in un elemento della lista (avro' una lista di liste)
+			is_function = 0
+			local_vars.append(list(tmp_local_vars))	#aggiungo alla lista la lista delle variabili locali di una funzione
+			if local_var_cnt > 0:
+				function_list.append(list(tmp_function_list))
+		if m_var:
+			#trovata una variabile, piu' avanti vedo se salvarla come variabile globale o locale (cioe' di una funzione)
+			if curr_line[m_var.end()] != ';' and curr_line[m_var.end()] != '\n':
+				tmp_string = ''
+				k = m_var.end()
+				while curr_line[k] == ' ' or curr_line[k] == '\t':
+					k = k + 1
+				while curr_line[k] != ';' and curr_line[k] != ' ' and curr_line[k] != '=':
+					#questa e' la variabile, la salvo tutta in tmp_string
+					tmp_string = tmp_string + curr_line[k]
+					k = k + 1
+				if len(tmp_string) > 0:
+					if is_function == 0:
+						#ho trovato una variabile globale
+						global_vars.append(tmp_string)
+					else:
+						#variabile locale di una funzione
+						tmp_local_vars.append(tmp_string)
+						local_var_cnt += 1
+						
+	#controllo se le variabili dichiarate sono usate
+	for gvar in global_vars:
+		gv_counter = 0
+		p = re.compile('\\b'+gvar+'\\b')
+		for line in lines:
+			m = p.search(line)
+			if m:
+				#print 'OK ', fun
+				gv_counter +=1
+		gvar_counter.append(gv_counter)
+		if gv_counter < 2:
+			print 'KO - Occorrenze per', gvar, ':', gv_counter
+	#controllo variabili locali
+	for i in range(0,len(function_list)):
+		for lvar in local_vars[i]:
+			lv_counter = 0
+			p = re.compile('\\b'+lvar+'\\b')
+			for line in function_list[i]:
+				m = p.search(line)	# ' '.join(function_list[i]
+				if m:
+					lv_counter += 1
+			lvar_counter.append(lv_counter)
+			if lv_counter < 2:
+				print 'KO - Occorrenze per', lvar, ':', lv_counter
+
 #______________________________
 
 count_functions_usage('b.js')
+print '******************'
+count_variables_usage()
 
 #______________________________
 
-global_vars = []
-local_vars = []
-p = re.compile('\\bfunction\\b')
-vr = re.compile('\\bvar\\b')
-block_end = re.compile('}')
-is_function = 0
-tmp_local_vars = []
-gvar_counter = []
-
-#cerco le variabili dichiarate
-for line in lines:
-	curr_line = line.strip()
-	m_var = vr.search(curr_line)
-	m_function = p.search(curr_line)
-	m_blockend = block_end.search(curr_line)
-	if m_function:
-		#da qui trovero' variabili locali
-		is_function = 1
-		del tmp_local_vars[:]	#pulisco la lista
-	if m_blockend:
-		#fine variaibli locali per una certa funzione, copio quelle che ho trovato in un elemento della lista (avro' una lista di liste)
-		is_function = 0
-		local_vars.append(list(tmp_local_vars))	#aggiungo alla lista la lista delle variabili locali di una funzione
-	if m_var:
-		#trovata una variabile, piu' avanti vedo se salvarla come variabile globale o locale (cioe' di una funzione)
-		if curr_line[m_var.end()] != ';' and curr_line[m_var.end()] != '\n':
-			tmp_string = ''
-			k = m_var.end()
-			while curr_line[k] == ' ' or curr_line[k] == '\t':
-				k = k + 1
-			while curr_line[k] != ';' and curr_line[k] != ' ' and curr_line[k] != '=':
-				#questa e' la variabile, la salvo tutta in tmp_string
-				tmp_string = tmp_string + curr_line[k]
-				k = k + 1
-			if len(tmp_string) > 0:
-				if is_function == 0:
-					#ho trovato una variabile globale
-					global_vars.append(tmp_string)
-				else:
-					#variabile locale di una funzione
-					tmp_local_vars.append(tmp_string)
-					
-#controllo se le variabili dichiarate sono usate
-for gvar in global_vars:
-	gv_counter = 0
-	p = re.compile('\\b'+gvar+'\\b')
-	for line in lines:
-		m = p.search(line)
-		if m:
-			#print 'OK ', fun
-			gv_counter +=1
-		gvar_counter.append(gv_counter)
-	if gv_counter < 2:
-		print 'KO - Occorrenze per', gvar, ':', gv_counter
-	#else:
-		#print 'OK', gvar, ':', gv_counter
-	
-print 'Global'
-for el in global_vars:
-	print el
-print 'Local'
-for el in local_vars:
-	print '---'
-	for el1 in el:
-		print el1
 
